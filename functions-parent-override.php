7cb2e5a16f53c749459f0bd6da353c95df7933c4
Dan Cameron <dan@sproutventure.com>
2013-01-11 10:16:02 -0800

    Force Login or Registration

--- functions-parent-override.php
+++ functions-parent-override.php
@@ -5,3 +5,28 @@
  *
  *
  */
+
+
+// redirect from homepage
+add_action( 'pre_gbs_head', 'gb_redirect_to_home' );
+/**
+ * Redirect to the latest deal
+ * @return null redirect
+ */
+function gb_redirect_to_home() {
+
+	// Redirect away from almost everything if the user is not logged in.
+	if ( !is_user_logged_in() && !is_home() ) {
+		if (
+			gb_on_login_page() ||
+			gb_on_reset_password_page() ||
+			gb_on_registration_page() 
+			) {
+				return;
+			} else {
+				gb_set_message( 'Membership Required.' );
+				gb_login_required();
+				return;
+			}
+	}
+}
