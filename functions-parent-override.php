1d13964e0b19f534137c6fc4cafb0218748c8306
Dan Cameron <dan@sproutventure.com>
2013-01-23 08:58:09 -0800

    Remove new Attributes.

--- functions-parent-override.php
+++ functions-parent-override.php
@@ -6,27 +6,7 @@
  *
  */
 
-
-// redirect from homepage
-add_action( 'pre_gbs_head', 'gb_redirect_to_home' );
-/**
- * Redirect to the latest deal
- * @return null redirect
- */
-function gb_redirect_to_home() {
-
-	// Redirect away from almost everything if the user is not logged in.
-	if ( !is_user_logged_in() && !is_home() ) {
-		if (
-			gb_on_login_page() ||
-			gb_on_reset_password_page() ||
-			gb_on_registration_page() 
-			) {
-				return;
-			} else {
-				gb_set_message( 'Membership Required.' );
-				gb_login_required();
-				return;
-			}
-	}
+add_action( 'init', 'init_hook', 100 );
+function init_hook() {
+	remove_filter( 'gb_add_to_cart_form_fields', array( 'Group_Buying_Attributes', 'filter_add_to_cart_add_category_selection' ) );
 }
