fd3767c9e19026d1bc0ad4279e35fbdaca523cb1
Dan Cameron <dan@sproutventure.com>
2013-08-11 09:28:14 -0700

    Pleaceholder Enhanced

--- functions-parent-override.php
+++ functions-parent-override.php
@@ -5,3 +5,26 @@
  *
  *
  */
+
+///////////////////
+// Theme Scripts //
+///////////////////
+
+function gbs_child_theme_register_scripts() {
+
+	if ( !is_admin() ) {
+		wp_register_script( 'placeholder-enhanced', get_stylesheet_directory().'/js/jquery.placeholder-enhanced.min.js', array( 'jquery' ), gb_ptheme_current_version(), false );
+	}
+
+}
+add_action( 'init', 'gbs_child_theme_register_scripts' );
+
+function gbs_child_theme_enqueue_scripts() {
+
+	if ( !is_admin() ) {
+
+		// GBS Scripts
+		wp_enqueue_script( 'placeholder-enhanced' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'gbs_child_theme_enqueue_scripts' );
