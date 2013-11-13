545c34def525c301ad423acefe25af0fb63bc60e
Dan Cameron <dan@sproutventure.com>
2013-11-13 07:57:07 -0800

    Adding custom page.

---
+++ checkout-template.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Checkout Page
+*/
+?>
+<?php get_header(); ?>
+<?php $checkout_page = Group_Buying_Checkouts::get_instance(); ?>
+
+
+<div id="account_page" class="clearfix">
+    
+    <div id="content_wrap" class="clearfix">
+        
+        <div id="content" class="full clearfix">
+        
+            <div class="page_title clearfix">
+                <h1 class="main_heading gb_ff"><span class="title_highlight"><?php gb_e('Checkout'); ?></span></h1>
+            </div>
+            <?php $checkout_page->view_checkout(); ?>
+        </div><!-- #content -->
+    
+    </div><!-- #content_wrap -->    
+    
+</div><!-- #single_deal -->
+
+<?php get_footer(); ?>
